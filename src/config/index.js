d34b24aeefa5aeae859d1658452990d70ebaa2c3
Jahirul83 <jahirulislamcse83@gmail.com>
2025-01-08 01:49:32 +0600

    common form config

---
+++ src/config/index.js
@@ -0,0 +1,24 @@
+export const registrationFormControls =[
+    {
+        name : 'userName',
+        label : 'User Name',
+        Placeholder : 'Enter your user name',
+        componentType : 'input',
+        type : 'text',
+    },
+    {
+        name : 'email',
+        label : 'Email',
+        Placeholder : 'Enter your email',
+        componentType : 'input',
+        type : 'Email',
+    },
+    {
+        name : 'password',
+        label : 'Password',
+        Placeholder : 'Enter your password',
+        componentType : 'input',
+        type : 'password',
+    },
+    
+]
--- src/pages/auth/register.jsx
+++ src/pages/auth/register.jsx
@@ -1,9 +1,24 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
+
+const initialState = {
+    userName: '',
+    email: '',
+    password: ''
+}
 
 const AuthRegister = () => {
+    const [formData, setFormData] = useState(initialState)
     return (
-        <div>
-            <h1>register</h1>
+        <div className="mx-auto w-full max-w-md space-y-6">
+            <div className="text-center">
+                <h1 className="text-3xl font-bold tracking-tight text-foreground">Create new account</h1>
+                <p>Already have an account?
+                    <Link className="font-medium ml-2 text-primary hover:underline" to={'/auth/login'}>login</Link>
+                </p>
+            </div>
+           {/* here start */}
         </div>
     );
 };
